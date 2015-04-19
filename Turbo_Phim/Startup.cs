c4b15d37461bd9cda33560c483955055da93742c
Uy Nguyễn <leesan.itus@gmail.com>
2015-04-19 22:28:55 +0700

    First commit

---
+++ Turbo_Phim/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Turbo_Phim.Startup))]
+namespace Turbo_Phim
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
