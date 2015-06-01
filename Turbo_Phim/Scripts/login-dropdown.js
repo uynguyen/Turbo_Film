80a577060ed3294168faa25af4a1a551afd93c84
Lê Quốc Vin <winlqvin.it@gmail.com>
2015-06-01 17:21:26 +0700

    gần hoàn thiện chức năng đăng nhập và đăng ký

---
+++ Turbo_Phim/Scripts/login-dropdown.js
@@ -0,0 +1,10 @@
+﻿
+function dropdownLogin(b) {
+    if (b == null) {
+        $("#login-message").html("Vui lòng đăng nhập trước đã!");
+        $("#login-dropdown").addClass('open');
+    }
+    else {
+        $("#login-message").html("Đăng nhập để viết bài và bình luận!");
+    }
+}
