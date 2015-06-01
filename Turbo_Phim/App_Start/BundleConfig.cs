73d5a532ce55a3c9b76c1641947dbfe97e618b5d
Uy Nguyễn <leesan.itus@gmail.com>
2015-06-02 01:24:12 +0700

    Fix conflit, bỏ các file jquey2.1.3

--- Turbo_Phim/App_Start/BundleConfig.cs
+++ Turbo_Phim/App_Start/BundleConfig.cs
@@ -7,34 +7,12 @@ namespace Turbo_Phim
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-<<<<<<< HEAD
+
 
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").IncludeDirectory("~/Scripts/", "*.js"));
-            //bundles.Add(new ScriptBundle("~/bundles/ckfinder").Include("~/ckfinder/ckfinder.js"));
-            //bundles.Add(new ScriptBundle("~/bundles/ckeditor").Include("~/ckeditor/ckeditor.js"));
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-=======
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js",
-            //            "~/Scripts/jquery-ui.js",
-            //            "~/Scripts/jquery-ui.min.js",
-            //            "~/Scripts/jquery.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
 
-            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
-            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js",
-            //          "~/Scripts/respond.js"));
-            bundles.Add(new ScriptBundle("~/bundles/jquery").IncludeDirectory("~/Scripts/", "*.js"));
->>>>>>> 14c573be6204f39af0baa542494024f789a525cf
             bundles.Add(new StyleBundle("~/Content/css").IncludeDirectory("~/Content/", "*.css"));
         
         }
