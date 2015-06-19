0d9c64e2a46d50f99f7c0f63136d666e0fa6c956
Lê Quốc Vin <winlqvin.it@gmail.com>
2015-06-19 17:44:15 +0700

    phân trang search

--- Turbo_Phim/Controllers/SearchController.cs
+++ Turbo_Phim/Controllers/SearchController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Turbo_Phim.Models;
 using Turbo_Phim.Services;
+using PagedList;
 
 namespace Turbo_Phim.Controllers
 {
@@ -20,24 +21,22 @@ namespace Turbo_Phim.Controllers
         {
             ViewBag.filmName = filmName;
 
-            ViewBag.HomeStatus = "inactive";
-            ViewBag.VideoStatus = "inactive";
-            ViewBag.ReviewStatus = "active";
-            ViewBag.ContactStatus = "inactive";
-            if (page == null)
-            {
-                if (TempData["currentPage"] != null)
-                {
-                    page = Int32.Parse(TempData["currentPage"].ToString());
-                }
-                else
-                {
-                    page = 1;
-                    TempData["currentPage"] = page;
-                }
-            }
-            else
-                TempData["currentPage"] = page;
+           
+            if (TempData["strSort"] == null)
+                TempData["strSort"] = "ID";
+
+            if (TempData["sortDirection"] == null)
+                TempData["sortDirection"] = "true";
+            FilmService phimService = new FilmService();
+
+            List<PhimViewModels> searchResult = phimService.searchFilm(filmName);
+            return View(searchResult);
+        }
+
+        public ActionResult PagingSimpleSearch(int? page, String filmName)
+        {
+            ViewBag.filmName = filmName;
+            
             if (TempData["strSort"] == null)
                 TempData["strSort"] = "ID";
 
@@ -46,13 +45,11 @@ namespace Turbo_Phim.Controllers
 
             FilmService phimService = new FilmService();
 
-            int maxPage;
-
             ViewBag.maxIndexPage = phimService.getMaxIndexPage();
 
             List<PhimViewModels> searchResult = phimService.searchFilm(filmName);
-         
-            return View(searchResult);
+
+            return PartialView(searchResult.ToPagedList(page ?? 1,10));
         }
 
         public ActionResult AdvanceSearch(int? page, String actor, String directer, String country, String type)
@@ -62,24 +59,6 @@ namespace Turbo_Phim.Controllers
             ViewBag.country = country;
             ViewBag.type = type;
 
-            ViewBag.HomeStatus = "inactive";
-            ViewBag.VideoStatus = "inactive";
-            ViewBag.ReviewStatus = "active";
-            ViewBag.ContactStatus = "inactive";
-            if (page == null)
-            {
-                if (TempData["currentPage"] != null)
-                {
-                    page = Int32.Parse(TempData["currentPage"].ToString()); //Chuyển hướng từ action delete
-                }
-                else
-                {
-                    page = 1;
-                    TempData["currentPage"] = page;
-                }
-            }
-            else
-                TempData["currentPage"] = page;
             if (TempData["strSort"] == null)
                 TempData["strSort"] = "ID";
             if (TempData["sortDirection"] == null)
@@ -88,8 +67,8 @@ namespace Turbo_Phim.Controllers
 
 
             FilmService phimService = new FilmService();
-            int maxPage;
-            ViewBag.maxIndexPage = phimService.getMaxIndexPage();
+ 
+     
 
             List<PhimViewModels> searchResult = phimService.searchFilm4(actor, directer, country, type);
         
@@ -99,6 +78,29 @@ namespace Turbo_Phim.Controllers
 
         }
 
+        public ActionResult PagingAdvanceSearch(int? page, String actor, String directer, String country, String type)
+        {
+            ViewBag.actor = actor;
+            ViewBag.directer = directer;
+            ViewBag.country = country;
+            ViewBag.type = type;
 
+          
+            if (TempData["strSort"] == null)
+                TempData["strSort"] = "ID";
+            if (TempData["sortDirection"] == null)
+                TempData["sortDirection"] = "true";
+
+
+
+            FilmService phimService = new FilmService();
+
+            List<PhimViewModels> searchResult = phimService.searchFilm4(actor, directer, country, type);
+
+
+            return PartialView(searchResult.ToPagedList(page??1, 10));
+
+
+        }
     }
 }
